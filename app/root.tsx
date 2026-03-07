b58fa2e9463bfe9a4500e2f824a900ad63449875
Sayeed  Shaikh <sayeedshaikh09@gmail.com>
2026-03-08 00:51:17 +0530

    adding caching v2

--- app/root.tsx
+++ app/root.tsx
@@ -88,49 +88,49 @@ export async function loader(args: Route.LoaderArgs) {
  * Load data necessary for rendering content above the fold. This is the critical data
  * needed to render the page. If it's unavailable, the whole page should 400 or 500 error.
  */
-async function loadCriticalData({ context }: Route.LoaderArgs) {
-  const { storefront } = context;
+// async function loadCriticalData({ context }: Route.LoaderArgs) {
+//   const { storefront } = context;
 
-  const [header] = await Promise.all([
-    storefront.query(HEADER_QUERY, {
-      cache: storefront.CacheLong(),
-      variables: {
-        headerMenuHandle: 'main-menu', // Adjust to your header menu handle
-      },
-    }),
-    // Add other queries here, so that they are loaded in parallel
-  ]);
+//   const [header] = await Promise.all([
+//     storefront.query(HEADER_QUERY, {
+//       cache: storefront.CacheLong(),
+//       variables: {
+//         headerMenuHandle: 'main-menu', // Adjust to your header menu handle
+//       },
+//     }),
+//     // Add other queries here, so that they are loaded in parallel
+//   ]);
 
-  return { header };
-}
+//   return { header };
+// }
 
 /**
  * Load data for rendering content below the fold. This data is deferred and will be
  * fetched after the initial page load. If it's unavailable, the page should still 200.
  * Make sure to not throw any errors here, as it will cause the page to 500.
  */
-function loadDeferredData({ context }: Route.LoaderArgs) {
-  const { storefront, customerAccount, cart } = context;
+// function loadDeferredData({ context }: Route.LoaderArgs) {
+//   const { storefront, customerAccount, cart } = context;
 
-  // defer the footer query (below the fold)
-  const footer = storefront
-    .query(FOOTER_QUERY, {
-      cache: storefront.CacheLong(),
-      variables: {
-        footerMenuHandle: 'footer', // Adjust to your footer menu handle
-      },
-    })
-    .catch((error: Error) => {
-      // Log query errors, but don't throw them so the page can still render
-      console.error(error);
-      return null;
-    });
-  return {
-    // cart: cart.get(),
-    // isLoggedIn: customerAccount.isLoggedIn(),
-    // footer,
-  };
-}
+//   // defer the footer query (below the fold)
+//   const footer = storefront
+//     .query(FOOTER_QUERY, {
+//       cache: storefront.CacheLong(),
+//       variables: {
+//         footerMenuHandle: 'footer', // Adjust to your footer menu handle
+//       },
+//     })
+//     .catch((error: Error) => {
+//       // Log query errors, but don't throw them so the page can still render
+//       console.error(error);
+//       return null;
+//     });
+//   return {
+//     // cart: cart.get(),
+//     // isLoggedIn: customerAccount.isLoggedIn(),
+//     // footer,
+//   };
+// }
 
 export function Layout({ children }: { children?: React.ReactNode }) {
   const nonce = useNonce();
