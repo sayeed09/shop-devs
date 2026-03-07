3060beb63a36922c6d9bd416285f3c53724fe8c9
Sayeed  Shaikh <sayeedshaikh09@gmail.com>
2026-03-08 00:22:26 +0530

    adding caching v2

--- app/routes/($locale).products.$handle.tsx
+++ app/routes/($locale).products.$handle.tsx
@@ -55,6 +55,8 @@ export async function loader(args: Route.LoaderArgs) {
       'Content-Type': 'application/json',
       // Cache for 1 hour, serve stale for 23 hours
       'Oxygen-Cache-Control': 'public, max-age=3600, stale-while-revalidate=82800',
+      'Cache-Control':
+        'public, s-maxage=3600, stale-while-revalidate=3600',
       Vary: 'Accept-Language, Accept-Encoding',
     },
   });
