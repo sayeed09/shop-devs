c5bb1f6011b31cf629391577aab6e1010d478702
Sayeed  Shaikh <sayeedshaikh09@gmail.com>
2026-03-08 00:58:07 +0530

    adding caching v2

--- app/routes/($locale).products.$handle.tsx
+++ app/routes/($locale).products.$handle.tsx
@@ -38,8 +38,7 @@ export async function loader(args: Route.LoaderArgs) {
   return Response.json(data, {
     headers: {
       "Cache-Control":
-        "public, max-age=0, s-maxage=1800, stale-while-revalidate=86400",
-      Vary: 'User-Agent',
+        "public, max-age=0, s-maxage=1800, stale-while-revalidate=86400"
     },
   });
 }
